0a4a28aeb763a0c32278aa971e52739223f624b2
Dnrk Charan <dnrkcharan@Dasaris-MacBook-Air.local>
2023-05-19 16:33:48 +0530

    added profile model

---
+++ app/database/models/my_profile.py
@@ -0,0 +1,20 @@
+import datetime
+from peewee import *
+from database.database_session import db
+from playhouse.postgres_ext import *
+
+class BaseModel(Model):
+    class Meta:
+        database = db
+
+class Profile(BaseModel):
+    userid = AutoField(PrimaryKeyField = True)
+    username = CharField(unique=True)
+    about = TextField()
+    first_name = CharField(unique=True)
+    last_name = CharField(unique=True)
+    created_date = DateTimeField(default=datetime.datetime.now)
+    updated_date = DateTimeField(default=datetime.datetime.now)
+
+    class Meta:
+        db_table = "profile"

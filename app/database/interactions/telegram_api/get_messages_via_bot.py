e38bda570505c840bc14e71c906b5b20e95ce985
dnrk <dnrk@Dasaris-MacBook-Air.local>
2023-06-20 01:50:57 +0530

    change accordingly.

--- app/database/interactions/telegram_api/get_messages_via_bot.py
+++ app/database/interactions/telegram_api/get_messages_via_bot.py
@@ -1,11 +1,32 @@
 import requests
+from config.env import BOT_AUTH_TOKEN
 
-def get_messages(text):
-    token = "6218429610:AAGlrVnRwq1RsvmuRjLOPaFKg54FxcptCVs"
-    username = "1786444932"
 
-    url = "https://api.telegram.org/bot" + token + "/sendMessage?chat_id=" + username + "&text=" + text
+def post_messages_to_users(message_to_channel, message_to_users):
+    try:
+        all_result=[]
+        get_users_url = "https://api.telegram.org/bot"+ BOT_AUTH_TOKEN + "/getUpdates"
+        get_users_list = requests.get(get_users_url)
+        get_all_users = get_users_list.json()
+        channel_list = []
+        user_list = []
 
-    result = requests.get(url)
-
-    return result.json()
+        for every_user in get_all_users["result"]:
+            if every_user.get("channel_post") and message_to_channel is not None and every_user["channel_post"]["sender_chat"]['id'] not in channel_list:
+                url = "https://api.telegram.org/bot" + BOT_AUTH_TOKEN + "/sendMessage?chat_id=" + str(every_user["channel_post"]["sender_chat"]['id']) + "&text=" + message_to_channel
+                result = requests.get(url)
+                result_ = result.json()
+                channel_list.append(every_user["channel_post"]["sender_chat"]['id'])
+            if every_user.get("message") and every_user["message"]["from"]['id'] == 750091829 and message_to_users is not None and every_user["message"]["from"]['id'] not in user_list:
+                url = "https://api.telegram.org/bot" + BOT_AUTH_TOKEN + "/sendMessage?chat_id=" + str(every_user["message"]["from"]['id']) + "&text=" + message_to_users
+                result = requests.get(url)
+                result_ = result.json()
+                user_list.append(every_user["message"]["from"]['id'])
+            else:
+                continue
+            
+            # all_result.append(result_)            
+        
+        return True
+    except:
+        raise
--- app/routers/telegram_routes.py
+++ app/routers/telegram_routes.py
@@ -1,17 +1,15 @@
 from fastapi import APIRouter
 from database.interactions.telegram_api.send_message_via_bot import telegram_message
-from database.interactions.telegram_api.get_messages_via_bot import get_messages
+from database.interactions.telegram_api.get_messages_via_bot import post_messages_to_users
 
 
 telegram_router = APIRouter()
 
 
-@telegram_router.get('/get_mesages_from_channel')
-def get_mesages_from_channel(text : str):
-    try:
-        return get_messages(text)
-    except:
-        return False
+@telegram_router.post('/get_mesages_from_channel')
+def get_mesages_from_channel(message_to_channel : str = None, message_to_users: str = None):
+    return post_messages_to_users(message_to_channel, message_to_users)
+
     
 
 @telegram_router.post('/post_bot_message')
